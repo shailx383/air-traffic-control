d6d92facdab283ebed04fcabf5e183622f9c5938
shailx383 <shailx383@gmail.com>
2023-04-14 18:33:18 +0530

    added plots folder and plots function

---
+++ plots.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+AGENT_MAP = {
+            'BEELINE': 0,
+            'RANDOM': 1,
+            'SARSA':  2,
+            'Q-LEARNING': 3,
+            'EXP SARSA':  4,
+        }
+
+def plot_performance(agent: str):
+    log_df = pd.read_csv('visitorlog.csv')
+    agent_log_df = log_df[log_df['agent'] == AGENT_MAP[agent]]
+    x = agent_log_df['id']
+    y = agent_log_df['score']
+    plt.figure()
+    plt.plot(x, y, 'r')
+    plt.xlabel('Episode')
+    plt.ylabel('Score')
+    plt.title(agent)
+    plt.savefig('plots/' + agent + '_performance.png')
+
+plot_performance("SARSA")
+    
