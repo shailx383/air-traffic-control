9f8c70bae7cd2b2a1931f48d9a0d47ce8ef66338
shailx383 <shailx383@gmail.com>
2023-04-16 10:12:58 +0530

    added plots and csv files

--- plots.py
+++ plots.py
@@ -81,7 +81,7 @@ def triple_plot_compared_performance(agent1: str, agent2: str, agent3: str, step
     plt.title('Performance of '+ agent1 +' vs ' + agent2 +' vs ' + agent3 + ': ' + str(len(x1))+ ' episodes')
     plt.savefig('plots/' + agent1 +'_vs_' + agent2 + '_vs_' + agent3 + '_performance.png')
 
-# triple_plot_compared_performance("SARSA", "BEELINE", "RANDOM", 25, 'visitorlog_s.csv', 'log.csv', 'log.csv')
+# triple_plot_compared_performance("SARSA", "EXP SARSA", "RANDOM", 35, 'visitorlog_s.csv', 'visitorlog_e.csv', 'visitorlog.csv')
 
 def time_plots(agent: str, csv_path: str, steps: int):
     log_df = pd.read_csv(csv_path)
@@ -102,3 +102,52 @@ def time_plots(agent: str, csv_path: str, steps: int):
 
 # time_plots("SARSA", 'visitorlog_s.csv', 10)
     
+def f(csv_path):
+    df = pd.read_csv(csv_path)
+    df['agent'] = 5
+    df.to_csv('test.csv')
+    
+# f('visitorlog_dq.csv')
+
+def all_plot(steps):
+    log_df0 = pd.read_csv('visitorlog.csv')
+    log_df1 = pd.read_csv('visitorlog.csv')
+    log_df2 = pd.read_csv('visitorlog_s.csv')
+    log_df3 = pd.read_csv('visitorlog_q.csv')
+    log_df4 = pd.read_csv('visitorlog_e.csv')
+    log_df5 = pd.read_csv('test.csv')
+    agent_log_df0 = log_df0[log_df0['agent'] == 0]
+    agent_log_df1 = log_df1[log_df1['agent'] == 1]
+    agent_log_df2 = log_df2[log_df2['agent'] == 2]
+    agent_log_df3 = log_df3[log_df3['agent'] == 3]
+    agent_log_df4 = log_df4[log_df4['agent'] == 4]
+    agent_log_df5 = log_df5[log_df5['agent'] == 5]
+    print(len(agent_log_df0), len(agent_log_df1), len(agent_log_df2), len(agent_log_df3), len(agent_log_df4), len(agent_log_df5))
+    y0 = agent_log_df0['score']
+    y1 = agent_log_df1['score']
+    y2 = agent_log_df2['score']
+    y3 = agent_log_df3['score']
+    y4 = agent_log_df4['score']
+    y5 = agent_log_df5['score']
+    x1 = list(range(1, min(len(y1), len(y0), len(y2), len(y3), len(y4), len(y5))+1))
+    plt.figure(figsize=(14,14))
+    avs0 = [sum(y0[i - steps:i])/len(y0[i-steps:i]) for i in range(steps, len(x1), steps)]
+    avs1 = [sum(y1[i - steps:i])/len(y1[i-steps:i]) for i in range(steps, len(x1), steps)]
+    avs2 = [sum(y2[i - steps:i])/len(y2[i-steps:i]) for i in range(steps, len(x1), steps)]
+    avs4 = [sum(y4[i - steps:i])/len(y4[i-steps:i]) for i in range(steps, len(x1), steps)]
+    avs5 = [sum(y5[i - steps:i])/len(y5[i-steps:i]) for i in range(steps, len(x1), steps)]
+    avs3 = [sum(y3[i - steps:i])/len(y3[i-steps:i]) for i in range(steps, len(x1), steps)]
+    avs_x = [i for i in range(steps, len(x1), steps)]
+    plt.plot(avs_x, avs0, 'r', label = 'BEELINE')
+    plt.plot(avs_x, avs1, 'b', label = 'RANDOM')
+    plt.plot(avs_x, avs2, 'g', label = 'SARSA')
+    plt.plot(avs_x, avs3, 'y', label = 'Q-LEARNING')
+    plt.plot(avs_x, avs4, 'c', label = 'EXP SARSA')
+    plt.plot(avs_x, avs5, 'm', label = 'DQ-LEARNING')
+    plt.legend(bbox_to_anchor = (12,12))
+    plt.xlabel('Episode')
+    plt.ylabel('Score')
+    plt.title('Performance of all agents: ' + str(len(x1))+ ' episodes')
+    plt.savefig('plots/performance.png')
+    
+all_plot(100)
